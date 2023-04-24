a02255a01b69641fd8d04a4b893d41b3552b5fa1
caio trauczynski <99267126+caiotr@users.noreply.github.com>
2023-04-24 09:09:48 -0300

    Add files via upload

---
+++ intrigued-cookies/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View,Image,} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+   
+      <View style={styles.navbar}>
+        <Text style={styles.navbarText}>Favorites</Text>
+         <Image
+style={styles.menu}
+source={require('./assets/menu.png')}
+/>
+      </View>
+      <View style={styles.content}>
+        <Image
+  style={styles.image}
+  source={require('./assets/pokemonicon.jpg') }
+/>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  menu:{
+  width:'20%',
+  height:'40%',
+  justifyContent: 'left',
+  alignItems: 'left',
+},
+  navbar: {
+    backgroundColor: 'green',
+    height: 50,
+ 
+  },
+  navbarText: {
+    color: 'white',
+    fontSize: 18,
+    justifyContent: 'center',
+  alignItems: 'center',
+  flex:1,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#5A5A5A',
+
+  },
+  image: {
+  width: '100%',
+  height: '20%',
+  justifyContent:'top',
+  alignItems:'top',
+},
+});
