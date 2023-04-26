a1f63b4631e52e027b9741d0de04d47ffb09c653
caio trauczynski <99267126+caiotr@users.noreply.github.com>
2023-04-26 08:37:25 -0300

    Add files via upload

---
+++ intrigued-cookies/components/pokemon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,Image,} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Image
+  style={styles.image}
+  source={require('./assets/pokemon.jpg') }
+/>
+      </View>
+    </View>
+  );
+}
